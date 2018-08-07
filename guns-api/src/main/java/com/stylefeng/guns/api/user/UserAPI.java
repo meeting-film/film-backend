76f9ef28f2e0f15889d34b81cf4baf67b392f372
jiangzh <jiangzh@sina.com>
2018-08-07 23:23:17 +0800

    first commit

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -0,0 +1,15 @@
+package com.stylefeng.guns.api.user;
+
+public interface UserAPI {
+
+    int login(String username, String password);
+
+    boolean register(UserModel userModel);
+
+    boolean checkUsername(String username);
+
+    UserInfoModel getUserInfo(int uuid);
+
+    UserInfoModel updateUserInfo(UserInfoModel userInfoModel);
+
+}
